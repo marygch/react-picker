8525d30fb5dae9a090c104834e58f3c498091372
marygch <maria.r.garcia.ch@gmail.com>
2020-09-08 23:36:46 -0500

    docs agregado

---
+++ docs/precache-manifest.a7af65808dd56b25efd13d72c2c44c2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a37ce44251e9a22c8574d696a0c5d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "910a49cd6d89450d5377",
+    "url": "/static/css/main.f64170a9.chunk.css"
+  },
+  {
+    "revision": "d4d1d6ce6c8d2a3e43d7",
+    "url": "/static/js/2.66a6280b.chunk.js"
+  },
+  {
+    "revision": "910a49cd6d89450d5377",
+    "url": "/static/js/main.9c540faa.chunk.js"
+  },
+  {
+    "revision": "7b6ae76f47ac5ba84b47",
+    "url": "/static/js/runtime-main.d0e95c3d.js"
+  }
+]);
